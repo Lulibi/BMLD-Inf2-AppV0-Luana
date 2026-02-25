4011ce64037de82ca87d56cddd357096639e9442
lulibi <birtolua@students.zhaw.ch>
2026-02-25 09:27:37 +0100

    Autor entfernt

--- views/home.py
+++ views/home.py
@@ -10,6 +10,4 @@ Diese App wurde von folgenden Personen entwickelt:
 - Luana Birtone (birtolua@students.zhaw.ch)
 
 Diese App ist das leere Gerüst für die App-Entwicklung im Modul Informatik 2 (BMLD/ZHAW)
-
-Autor: Samuel Wehrli (wehs@zhaw.ch)
 """
